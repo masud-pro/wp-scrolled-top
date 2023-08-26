a8b1dce51acfdd17f0c5e8695503cdbf5e61be8f
masudrana03 <920mash@gmail.com>
2023-08-26 22:41:00 +0600

    plagin set up done.

---
+++ wp-scrolled-top.php
@@ -0,0 +1,25 @@
+<?php 
+
+/**
+ * Plugin Name:       Scroll To Top
+ * Plugin URI:        https://masudrana.pro/scroll-to-top
+ * Description:       This is my first test flight plugin for thw word press.
+ * Version:           0.1.0
+ * Requires at least: 6.0
+ * Requires PHP:      8.1
+ * Author:            Md.Masud Rana
+ * Author URI:        https://masudrana.pro/
+ * Text Domain:       stt
+ * Updated URI:       https://masudrana.pro
+ * 
+ * @package Scroll To Top
+ */
+
+
+
+
+
+
+
+
+?>
